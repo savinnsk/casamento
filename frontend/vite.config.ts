9bac73783ed50b5985f6d68563a8703b0048e4c9
Savio Picanço <saviopbrito@outlook.com>
2024-10-03 03:31:08 -0300

    add https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://203.0.113.0:3002', // URL do seu back-end
+        changeOrigin: true,
+        secure: false, // Permite conexões não seguras
+      },
+    },
+  },
+});
