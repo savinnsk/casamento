68f95039c9ce2d91179f39db3823b0f09d85bfbd
Savio Picanço <saviopbrito@outlook.com>
2024-10-03 03:48:18 -0300

    add front to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 //import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as serveStatic from 'serve-static';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+ // const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Servir arquivos estáticos
+  app.use(serveStatic(join(__dirname, '..', 'front')));
+
+  // Qualquer outra configuração que você precise
+  app.enableCors(); // Se precisar
+
 
   const config = new DocumentBuilder()
     .setTitle('API FOR MARRIEGE')
