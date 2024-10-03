d917ce21491bf269168f4df363c2fc24de62c80f
Savio Picanço <saviopbrito@outlook.com>
2024-10-03 03:58:10 -0300

    add front to backend

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -4,7 +4,7 @@ import { useStore } from "../../hooks/store"
 import "./index.css"
 
 export function Header(){
-    const {user,setUserPage,setHomePage} = useStore()
+    const {setUserPage,setHomePage} = useStore()
     return <div className="header"> 
     <span onClick={()=> {setUserPage(false) ,setHomePage(true)}}> Inicio </span> 
     <span  onClick={()=> {setUserPage(true) ,setHomePage(false)}}> Meus Presentes🎁 </span> </div>
--- frontend/src/service/axios.ts
+++ frontend/src/service/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-        baseURL: 'http://18.188.156.208:3002', 
+        baseURL: 'https://casamento.savinnsk.com', 
         timeout: 5000,
     });
