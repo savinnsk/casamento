54e9dd76d924d4d6bcc0a5968082bb86fece94f6
Savio Picanço <saviopbrito@outlook.com>
2024-10-03 03:25:57 -0300

    add https

--- frontend/src/service/axios.ts
+++ frontend/src/service/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-        baseURL: 'http://18.188.156.208:3002', 
+        baseURL: 'https://18.188.156.208:3002', 
         timeout: 5000,
     });
